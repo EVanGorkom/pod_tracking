ca61d01a834784023ea51542341b9bd2c8141461
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-08-04 14:35:56 -0500

    Modify the logic and routes for POST endpoint of the Card model

--- v1/views/card_views.py
+++ v1/views/card_views.py
@@ -3,26 +3,51 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from ..services.card_service import CardService
-# from ..serializers import CardSerializer
+from ..serializers import CardSerializer
 from ..models.card_model import Card
+from ..models.color_model import Color
 
-def find_cards(request):
+COLORS = {
+    "W": "White",
+    "U": "Blue",
+    "B": "Black",
+    "R": "Red",
+    "G": "Green",
+    "C": "Colorless"
+}
+
+# todo: build this service as a POST which expects a specific card name from the FE. FE should utilize the
+  # auto-complete route to find and preview cards before clicking a 'save' button which sends the POST request to the backend with the name of the card to search for/save.
+
+@api_view(["POST"])
+def create_card(request):
   query = request.GET.get('query')
   if query:
-    cards_data = CardService.search_cards(query)
-    for details in cards_data:
+    card_data = CardService.search_cards(query)
+    for data in card_data:
       card = Card.objects.create(
-        name=details.get('name'),
-        mana_cost=details.get('mana_cost'),
-        cmc=details.get('cmc'),
-        # color=details.get('color'),
-        type_line=details.get('type_line'),
-        rarity=details.get('rarity'),
-        cmdr_legal=details.get('legalities').get('commander'),
-        img=details.get('image_uris').get('normal'),
-        purchase_uris=details.get('purchase_uris').get('tcgplayer')
+        name=data.get('name'),
+        mana_cost=data.get('mana_cost'),
+        cmc=data.get('cmc'),
+        type_line=data.get('type_line'),
+        rarity=data.get('rarity'),
+        cmdr_legal=data.get('legalities').get('commander'),
+        img=data.get('image_uris').get('normal'),
+        purchase_uris=data.get('purchase_uris').get('tcgplayer')
       )
+
+      if len(data.get('colors_identity')) == 0:
+          colors = ["C"]
+      colors = data.get('colors_identity', [])
+
+      for color_abbreviation in colors:
+          color = Color.objects.filter(name=COLORS[color_abbreviation]).first()
+          if color:
+              card.colors.add(color)
       card.save()
-    return JsonResponse({'message': 'Card populated successfully'})
+
+    serializer = CardSerializer(card)
+
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
   else:
-    return JsonResponse({'error': 'Query parameter is required'}, status=400)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
