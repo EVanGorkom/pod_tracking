60eeaa0a47d3b66c20a32f914ed08d55fb98c708
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-08-04 14:36:21 -0500

    Update the admin file for v1 to account for the new tables

--- v1/admin.py
+++ v1/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models.card_model import Card
+from .models.color_model import Color
+from .models.card_color_model import CardColor
 
 admin.site.register(Card)
+admin.site.register(Color)
+admin.site.register(CardColor)
 
 # Register your models here.
